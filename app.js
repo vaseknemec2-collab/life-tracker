44644a067684dc6e5e4215b5db540375f349c7b0
vaseknemec2-collab <vaseknemec2@gmail.com>
2026-05-30 13:44:20 +0200

    add daily reset + localStorage system

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+let today = new Date().toDateString();
+let savedDate = localStorage.getItem("date");
+
+if (savedDate !== today) {
+    localStorage.setItem("water", 0);
+    localStorage.setItem("reading", 0);
+    localStorage.setItem("date", today);
+}
 
 function addTask() {
     if (inputBox.value.trim() === '') {
         alert("you must write something");
-        return;
         inputBox.value = "";
+        return;
     } else {
         let li = document.createElement("li");
         li.textContent  = inputBox.value;
@@ -41,3 +49,40 @@ inputBox.addEventListener("keydown", function(e) {
     }
 });
 showTask();
+
+let water = Number(localStorage.getItem("water")) || 0;
+    document.getElementById("waterValue").textContent = water;
+let reading = Number(localStorage.getItem("reading")) || 0;
+    document.getElementById("readingValue").textContent = reading;
+
+function addWater() {
+    water += 250;
+    document.getElementById("waterValue").textContent = water;
+
+    localStorage.setItem("water", water);
+
+    if (water >= 2500) {
+        document.querySelector(".habit-water").classList.add("done");
+    } else {
+        document.querySelector(".habit-water").classList.remove("done");
+    }
+}
+
+function addReading() {
+    reading += 5;
+    document.getElementById("readingValue").textContent = reading;
+
+    localStorage.setItem("reading", reading);
+
+    if (reading >= 30) {
+        document.querySelector(".habit-reading").classList.add("done");
+    } else
+    if (reading < 30) {
+        document.querySelector(".habit-reading").classList.remove("done");
+    }
+}
+
+function gymDone() {
+    document.querySelector(".habit-gym").classList.toggle("done");
+}
+
